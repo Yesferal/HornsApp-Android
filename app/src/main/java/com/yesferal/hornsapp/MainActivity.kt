c57d1d4a2905ddb7c310b76d063b9a0b40a206cc
Yesferal Cueva <yesferal@gmail.com>
2020-05-30 06:06:37 -0500

    Add hornsapp-lib.cpp

--- app/src/main/java/com/yesferal/hornsapp/MainActivity.kt
+++ app/src/main/java/com/yesferal/hornsapp/MainActivity.kt
@@ -11,20 +11,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        sample_text.text = secret()
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun secret(): String
 
     companion object {
 
         // Used to load the 'native-lib' library on application startup.
         init {
-            System.loadLibrary("native-lib")
+            System.loadLibrary("hornsapp-lib")
         }
     }
 }
