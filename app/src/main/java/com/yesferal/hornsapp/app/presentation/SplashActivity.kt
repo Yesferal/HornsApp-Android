f4f02353725bb4b07747ab3f161666c67ec05f21
Yesferal Cueva <yesferal@gmail.com>
2020-07-20 01:28:34 -0500

    Add SplashActivity

---
+++ app/src/main/java/com/yesferal/hornsapp/app/presentation/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.yesferal.hornsapp.app.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yesferal.hornsapp.app.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
