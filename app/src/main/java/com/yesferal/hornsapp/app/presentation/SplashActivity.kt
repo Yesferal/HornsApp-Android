86654438194ee24b069deb97c06e0d22b11faeff
Yesferal Cueva <yesferal@gmail.com>
2020-07-20 03:10:32 -0500

    Add animation in SplashActivity Layout

--- app/src/main/java/com/yesferal/hornsapp/app/presentation/SplashActivity.kt
+++ app/src/main/java/com/yesferal/hornsapp/app/presentation/SplashActivity.kt
@@ -14,5 +14,6 @@ class SplashActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+        overridePendingTransition(R.anim.fade_in, 0)
     }
 }
