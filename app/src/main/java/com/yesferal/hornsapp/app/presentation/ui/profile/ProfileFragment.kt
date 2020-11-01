3f022f4c119f3b1616d0924f0533e0964c4e418a
Yesferal Cueva <yesferal@gmail.com>
2020-11-01 01:53:50 -0500

    Enable Share option in ProfileFragment

--- app/src/main/java/com/yesferal/hornsapp/app/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yesferal/hornsapp/app/presentation/ui/profile/ProfileFragment.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.pm.PackageInfoCompat
 import androidx.fragment.app.Fragment
 import com.yesferal.hornsapp.app.R
-import com.yesferal.hornsapp.app.presentation.ui.favorite.FavoriteActivity
 import kotlinx.android.synthetic.main.fragment_setting.*
 
 class ProfileFragment
@@ -28,32 +28,40 @@ class ProfileFragment
 
         setUpVersion()
 
-        favoriteTextView.setImageView(R.drawable.ic_favorite)
-        favoriteTextView.setText(getString(R.string.favorites))
-        favoriteTextView.setOnClickListener {
-            val intent = Intent(activity, FavoriteActivity::class.java)
+        setUpShare()
+    }
+
+    private fun setUpShare() {
+        shareTextView.setImageView(R.drawable.ic_information)
+        shareTextView.setText(getString(R.string.share_with_friends))
+        shareTextView.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_hornsapp))
+            intent.type = "text/plain"
             startActivity(intent)
         }
     }
 
     private fun setUpVersion() {
-        activity?.let {
-            val packageInfo: PackageInfo = it
+        activity?.let { activityNonNull ->
+            val packageInfo: PackageInfo = activityNonNull
                 .packageManager
-                .getPackageInfo(it.packageName, 0)
+                .getPackageInfo(activityNonNull.packageName, 0)
 
             val versionName: String = packageInfo.versionName
-            val versionCode: Long = PackageInfoCompat.getLongVersionCode(packageInfo)
 
-            val version = "$versionName.$versionCode"
             versionTextView.setImageView(R.drawable.ic_information)
-            versionTextView.setText(getString(R.string.version), version)
+            versionTextView.setText(getString(R.string.version), versionName)
+            versionTextView.setOnClickListener {
+                val versionCode: Long = PackageInfoCompat.getLongVersionCode(packageInfo)
+                Toast.makeText(activityNonNull, versionCode.toString(), Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
     companion object {
-        fun newInstance() : ProfileFragment {
-            return ProfileFragment()
-        }
+        fun newInstance() = ProfileFragment()
     }
 }
